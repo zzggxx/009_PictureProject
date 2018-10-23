122212efa204eb041154d7872227c3df573f80a8
Will <will.g.zhou@magugi.com>
2018-10-23 15:13:24 +0800

    Initial commit

---
+++ app/src/main/java/com/example/will/pictureproject/MainActivity.java
@@ -0,0 +1,117 @@
+package com.example.will.pictureproject;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.LruCache;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private LruCache<String, Bitmap> mMemoryCache;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageView = (ImageView) findViewById(R.id.imageview);
+//        imageView.setImageResource(R.drawable.master);
+        imageView.setImageBitmap(
+                decodeSampledBitmapFromResource(getResources(), R.drawable.master, 1080, 1080));
+
+    }
+
+    /*------------------------------1,压缩减体积的方法处理图片---------------------------------------------*/
+    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId, int reqWidth, int reqHeight) {
+        // 第一次解析将inJustDecodeBounds设置为true，来获取图片大小
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+        // 调用上面定义的方法计算inSampleSize值
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+        // 使用获取到的inSampleSize值再次解析图片
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
+
+
+    public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // 源图片的高度和宽度
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+        if (height > reqHeight || width > reqWidth) {
+            // 计算出实际宽高和目标宽高的比率
+            final int heightRatio = Math.round((float) height / (float) reqHeight);
+            final int widthRatio = Math.round((float) width / (float) reqWidth);
+            // 选择宽和高中最小的比率作为inSampleSize的值，这样可以保证最终图片的宽和高
+            // 一定都会大于等于目标的宽和高。
+            inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+        }
+        return inSampleSize;
+    }
+
+    /*------------------------------2,LruCache的方法处理图片(给定缓存大小但是不压缩图片)----------*/
+    public void makeLruCache() {
+
+        int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+
+        // 使用最大可用内存值的1/8作为缓存的大小。
+        int cacheSize = maxMemory / 8;
+
+        // 重写此方法来衡量每张图片的大小，默认返回图片数量。
+        mMemoryCache = new LruCache<String, Bitmap>(cacheSize) {
+            @Override
+            protected int sizeOf(String key, Bitmap bitmap) {
+                // 重写此方法来衡量每张图片的大小，默认返回图片数量。
+                return bitmap.getByteCount() / 1024;
+            }
+        };
+
+    }
+
+    public void addBitmapToMemoryCache(String key, Bitmap bitmap) {
+        if (getBitmapFromMemCache(key) == null) {
+            mMemoryCache.put(key, bitmap);
+        }
+    }
+
+    public Bitmap getBitmapFromMemCache(String key) {
+        return mMemoryCache.get(key);
+    }
+
+    public void loadBitmap(int resId, ImageView imageView) {
+        final String imageKey = String.valueOf(resId);
+        final Bitmap bitmap = getBitmapFromMemCache(imageKey);
+        if (bitmap != null) {
+            imageView.setImageBitmap(bitmap);
+        } else {
+            imageView.setImageResource(R.drawable.master);
+            BitmapWorkerTask task = new BitmapWorkerTask(imageView);
+            task.execute(resId);
+        }
+    }
+
+    class BitmapWorkerTask extends AsyncTask<Integer, Void, Bitmap> {
+
+        public BitmapWorkerTask(ImageView imageView) {
+
+        }
+
+        // 在后台加载图片。
+        @Override
+        protected Bitmap doInBackground(Integer... params) {
+            final Bitmap bitmap = decodeSampledBitmapFromResource(
+                    getResources(), params[0], 100, 100);
+            addBitmapToMemoryCache(String.valueOf(params[0]), bitmap);
+            return bitmap;
+        }
+    }
+
+
+
+}
