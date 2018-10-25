848f2ccea2385f9e899e1a319bd5b2dff92d8f95
Will <will.g.zhou@magugi.com>
2018-10-25 15:14:15 +0800

    glide一些参数的讲解

--- glidemoudle/src/main/java/com/example/glidemoudle/MainActivity.java
+++ glidemoudle/src/main/java/com/example/glidemoudle/MainActivity.java
@@ -30,10 +30,15 @@ public class MainActivity extends AppCompatActivity {
 
     private void loadImg() {
 
-        //上下文,fragment,activity(销毁则销毁了,可能会导致空指针),application.context()(生命周期内一直加载)
+        /**
+         * 上下文,fragment,activity(销毁则销毁了,可能会导致空指针),application.context()(生命周期内一直加载)
+         * 第一个with()方法的源码还是比较好理解的。其实就是为了得到一个RequestManager对象而已，
+         * 然后Glide会根据我们传入with()方法的参数来确定图片加载的生命周期
+         */
         Glide.with(this)
 
                 //load(),可以从url,uri,recourse,路径等地方加载.
+                //最终load()方法返回的其实就是一个DrawableTypeRequest对象,从哪里加载和加载的格式的判断
                 .load(img_path_gif)
 
                 //自动判定图片的格式,是gif图还是静态图,若是动态的asBitmap()可取gif第一帧变为静态图,但是静态不能变为gif(会加载失败).
@@ -46,7 +51,14 @@ public class MainActivity extends AppCompatActivity {
                 .placeholder(R.drawable.place_holder)
                 .error(R.drawable.place_error)
 
-                //缓存策略设置(不缓存每一次都从网络加载)
+                //内存的缓存策略(true不使用内存缓存,若是不禁用默认就是开启的)
+                //.skipMemoryCache(true)
+
+                //本地缓存策略设置(不缓存每一次都从网络加载)
+                //DiskCacheStrategy.NONE：表示不缓存任何内容。
+                //DiskCacheStrategy.SOURCE：表示只缓存原始图片。
+                //DiskCacheStrategy.RESULT：表示只缓存转换过后的图片（默认选项）。
+                //DiskCacheStrategy.ALL ：表示既缓存原始图片，也缓存转换过后的图片
                 .diskCacheStrategy(DiskCacheStrategy.NONE)
 
                 //指定放置位置
