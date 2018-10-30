42f18b852c9cd8166e136371516cfb517966f8e6
Will <will.g.zhou@magugi.com>
2018-10-30 15:36:50 +0800

    添加文件夹的大小计算和文件夹的删除

---
+++ glidemoudle_3/src/main/java/com/example/glidemoudle_3/CommonUtils.java
@@ -0,0 +1,87 @@
+package com.example.glidemoudle_3;
+
+import java.io.File;
+
+public class CommonUtils {
+
+    // 递归方式 计算文件的大小
+    public static long getTotalSizeOfFilesInDir(final File file) {
+        if (file.isFile())
+            return file.length();
+        final File[] children = file.listFiles();
+        long total = 0;
+        if (children != null)
+            for (final File child : children)
+                total += getTotalSizeOfFilesInDir(child);
+        return total;
+    }
+
+    public static boolean deleteFolder(String sPath) {
+        boolean flag = false;
+        File file = new File(sPath);
+        // 判断目录或文件是否存在
+        if (!file.exists()) {  // 不存在返回 false
+            return flag;
+        } else {
+            // 判断是否为文件
+            if (file.isFile()) {  // 为文件时调用删除文件方法
+                return deleteFile(sPath);
+            } else {  // 为目录时调用删除目录方法
+                return deleteDirectory(sPath);
+            }
+        }
+    }
+
+    public static boolean deleteFile(String sPath) {
+        boolean flag = false;
+        File file = new File(sPath);
+        // 路径为文件且不为空则进行删除
+        if (file.isFile() && file.exists()) {
+            file.delete();
+            flag = true;
+        }
+        return flag;
+    }
+
+    /**
+     * 删除目录以及目录下的文件
+     *
+     * @param sPath 被删除目录的路径
+     * @return 目录删除成功返回true，否则返回false
+     */
+    public static boolean deleteDirectory(String sPath) {
+        //如果sPath不以文件分隔符结尾，自动添加文件分隔符
+        if (!sPath.endsWith(File.separator)) {
+            sPath = sPath + File.separator;
+        }
+        File dirFile = new File(sPath);
+        //如果dir对应的文件不存在，或者不是一个目录，则退出
+        if (!dirFile.exists() || !dirFile.isDirectory()) {
+            return false;
+        }
+        boolean flag = true;
+        //删除文件夹下的所有文件(包括子目录)
+        File[] files = dirFile.listFiles();
+        for (int i = 0; i < files.length; i++) {
+            //删除子文件
+            if (files[i].isFile()) {
+                flag = deleteFile(files[i].getAbsolutePath());
+                if (!flag) break;
+            } else {
+                //删除子目录
+                flag = deleteDirectory(files[i].getAbsolutePath());
+                if (!flag) break;
+            }
+        }
+        if (!flag) {
+            return false;
+        }
+        //删除当前目录
+        if (dirFile.delete()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
--- glidemoudle_3/src/main/java/com/example/glidemoudle_3/custom/MyGlideModule.java
+++ glidemoudle_3/src/main/java/com/example/glidemoudle_3/custom/MyGlideModule.java
@@ -7,6 +7,7 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.GlideBuilder;
 import com.bumptech.glide.Registry;
 import com.bumptech.glide.load.engine.cache.ExternalCacheDiskCacheFactory;
+import com.bumptech.glide.load.engine.cache.InternalCacheDiskCacheFactory;
 import com.bumptech.glide.module.GlideModule;
 
 /**
@@ -22,8 +23,18 @@ public class MyGlideModule implements GlideModule {
 
     @Override
     public void applyOptions(Context context, GlideBuilder builder) {
-//        默认路径:sdcard/Android/包名/cache/image_manager_disk_cache
+
+//        默认路径更改为外部存储
         builder.setDiskCache(new ExternalCacheDiskCacheFactory(context, DISK_CACHE_SIZE));
+//        默认路径:sdcard/Android/包名/cache/image_manager_disk_cache
+        builder.setDiskCache(new InternalCacheDiskCacheFactory(context, DISK_CACHE_SIZE));
+
+        //配置内存缓存大小 10MB,Glide内部已经通过一些列的辨别计算出来的,不应该自己再去画蛇添足
+//        builder.setMemoryCache(new LruResourceCache(10 * 1024 * 1024));
+        //配置图片池大小   20MB
+//        builder.setBitmapPool(new LruBitmapPool(20 * 1024 * 1024));
+
+
     }
 
     @Override
